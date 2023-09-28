f6ab3ec50457e7e17657c636fd4f0cd9cba86c29
arya sharma <sharmaaarya115@gmail.com>
2023-09-28 15:22:37 +0530

    haha

--- api/index.js
+++ api/index.js
@@ -1,10 +1,23 @@
-const express = require("express");
-const app = express();
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const db_js_1 = __importDefault(require("./config/db.js"));
+const user_routes_js_1 = __importDefault(require("./routes/user.routes.js"));
+const cors_1 = __importDefault(require("cors"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)({ origin: true }));
+app.use("/api/user", user_routes_js_1.default);
+(0, db_js_1.default)();
+const PORT = process.env.PORT || 4000;
 app.get("/", (_req, res) => {
-  res.json("Working");
+    res.json("Working");
 });
-
-app.listen(4000);
-
-module.exports = app;
+app.listen(PORT, () => {
+    console.log(`Listening to port ${PORT}`);
+});
+exports.default = app;
