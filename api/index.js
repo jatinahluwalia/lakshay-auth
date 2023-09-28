3e0f30a4d9072462f05d0a1aab52fd97a2a673a2
arya sharma <sharmaaarya115@gmail.com>
2023-09-28 15:31:46 +0530

    hahaa

--- api/index.js
+++ api/index.js
@@ -9,12 +9,10 @@ const db_js_1 = __importDefault(require("./config/db.js"));
 const user_routes_js_1 = __importDefault(require("./routes/user.routes.js"));
 const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
-const app = (0, express_1.default)();
-app.use(express_1.default.json({ limit: "100mb" }));
-app.use(express_1.default.urlencoded({ limit: "100mb", extended: true }));
-app.use((0, cors_1.default)({ origin: true }));
-app.use("/api", user_routes_js_1.default);
 (0, db_js_1.default)();
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use("/api", user_routes_js_1.default);
 const PORT = process.env.PORT || 4000;
 app.get("/", (_req, res) => {
     res.json("Working");
--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,10 @@ import userRouter from "./routes/user.routes.js";
 import cors from "cors";
 
 dotenv.config();
-const app = express();
-app.use(express.json({ limit: "100mb" }));
-app.use(express.urlencoded({ limit: "100mb", extended: true }));
-app.use(cors({ origin: true }));
-app.use("/api", userRouter);
-
 connectToDB();
+const app = express();
+app.use(cors());
+app.use("/api", userRouter);
 
 const PORT = process.env.PORT || 4000;
 
